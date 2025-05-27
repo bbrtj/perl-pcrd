28dbf3a0a642abe5d17074d715a68107b1e11043
bbrtj <bbrtj.pro@gmail.com>
2025-05-27 21:11:50 +0200

    Do not try to load config if the config file is not available

--- lib/PCRD/Config/File.pm
+++ lib/PCRD/Config/File.pm
@@ -33,34 +33,36 @@ sub dump_config
 sub load_config
 {
 	my ($self) = @_;
-
-	my @lines = PCRD::Util::slurp($self->filename);
 	my %conf;
 
-	chomp @lines;
-	foreach my $line (@lines) {
-		next unless $line =~ /\S/;
-		next if $line =~ /^\s*#/;
-		my ($key, $value) = split /=/, $line, 2;
+	if (-f $self->filename) {
+		my @lines = PCRD::Util::slurp($self->filename);
+		chomp @lines;
 
-		die "invalid configuration line: $line (no value)"
-			unless defined $value;
+		foreach my $line (@lines) {
+			next unless $line =~ /\S/;
+			next if $line =~ /^\s*#/;
+			my ($key, $value) = split /=/, $line, 2;
 
-		$key = PCRD::Util::trim($key);
-		$value = PCRD::Util::trim($value);
-		my @key_parts = split /\./, $key;
-		$key = pop @key_parts;
+			die "invalid configuration line: $line (no value)"
+				unless defined $value;
 
-		my $current_conf = \%conf;
-		foreach my $kp (@key_parts) {
-			die "invalid configuration: $line (path element $kp exists and not a hash)"
-				if exists $current_conf->{$kp} && ref $current_conf->{$kp} ne 'HASH';
-			$current_conf = $current_conf->{$kp} //= {};
+			$key = PCRD::Util::trim($key);
+			$value = PCRD::Util::trim($value);
+			my @key_parts = split /\./, $key;
+			$key = pop @key_parts;
+
+			my $current_conf = \%conf;
+			foreach my $kp (@key_parts) {
+				die "invalid configuration: $line (path element $kp exists and not a hash)"
+					if exists $current_conf->{$kp} && ref $current_conf->{$kp} ne 'HASH';
+				$current_conf = $current_conf->{$kp} //= {};
+			}
+
+			die "duplicated configuration: $line (key exists)"
+				if exists $current_conf->{$key};
+			$current_conf->{$key} = $value;
 		}
-
-		die "duplicated configuration: $line (key exists)"
-			if exists $current_conf->{$key};
-		$current_conf->{$key} = $value;
 	}
 
 	$self->_set_values(\%conf);
