4c7c1a717d61c6eb78733c5eb393eaad9350401a
bbrtj <bbrtj.pro@gmail.com>
2025-12-13 14:20:15 +0100

    Add fast variant of pcrctl

---
+++ src/pcrctl_fast.pas
@@ -0,0 +1,179 @@
+program pcrctl_fast;
+
+{$mode objfpc}{$H+}{$J-}
+
+uses
+  SysUtils, Sockets, BaseUnix;
+
+{ Need to be kept in sync with PCRD::Protocol }
+const
+	CSeparator = #9;
+	CSuccess = 'ok';
+	CError = 'nok';
+	CHandshake = '+';
+	CTerminator = #10'---'#10;
+	CTrue = 'true';
+	CFalse = 'false';
+
+type
+	TProgramArgs = record
+		Module: String;
+		Feature: String;
+		Value: String;
+	end;
+
+	ESocket = class(Exception)
+	private
+		FSocketError: LongInt;
+		function StringifyError(): String;
+	public
+		constructor Create(const Ex: String);
+		function ToString(): String; override;
+	end;
+
+{ implementation }
+
+function ESocket.StringifyError(): String;
+begin
+	case FSocketError of
+		EsockADDRINUSE: result := 'Socket address is already in use';
+		EsockEACCESS: result := 'Access forbidden';
+		EsockEBADF: result := 'Bad file descriptor';
+		EsockEFAULT: result := 'An error occurred';
+		EsockEINTR: result := 'Operation interrupted';
+		EsockEINVAL: result := 'Invalid value specified';
+		EsockEMFILE: result := 'Error code ?';
+		EsockEMSGSIZE: result := 'Wrong message size error';
+		EsockENOBUFS: result := 'No buffer space available';
+		EsockENOTCONN: result := 'Not connected';
+		EsockENOTSOCK: result := 'File descriptor is not a socket';
+		EsockEPROTONOSUPPORT: result := 'Protocol not supported';
+		EsockEWOULDBLOCK: result := 'Operation would block';
+		otherwise result := 'Unknown error';
+	end;
+end;
+
+constructor ESocket.Create(const Ex: String);
+begin
+	inherited Create(Ex);
+	FSocketError := SocketError;
+end;
+
+function ESocket.ToString(): String;
+begin
+	result := inherited ToString + ', ' + self.StringifyError;
+end;
+
+{ implementation end }
+
+function ReadArguments(): TProgramArgs;
+var
+	I: Int32;
+
+	function HasNextParam(): Boolean;
+	begin
+		result := I < ParamCount();
+	end;
+
+	function NextParam(): String;
+	begin
+		I += 1;
+
+		if I > ParamCount() then
+			raise Exception.Create('Invalid command line parameters');
+
+		result := ParamStr(I);
+	end;
+
+begin
+	I := 0;
+
+	result.Module := NextParam();
+	result.Feature := NextParam();
+
+	if HasNextParam() then
+		result.Value := NextParam();
+end;
+
+function SocketConnect(Address: String): TSocket;
+var
+	LAddr: TUnixSockAddr;
+begin
+	result := fpSocket(AF_UNIX, SOCK_STREAM, 0);
+	LAddr.family := AF_UNIX;
+	StrPCopy(@LAddr.path, Address);
+
+	try
+		if result = -1 then
+			raise ESocket.Create('Error creating socket');
+
+		if fpConnect(result, @LAddr, SizeOf(LAddr)) = -1 then
+			raise ESocket.Create('Error connecting');
+	except
+		on Ex: ESocket do begin
+			fpClose(result);
+			raise Ex;
+		end;
+	end;
+end;
+
+procedure SendQuery(Socket: TSocket; Args: TProgramArgs);
+	procedure SocketSend(Data: String);
+	begin
+		Data += CTerminator;
+		if Length(Data) <> fpSend(Socket, @Data[1], Length(Data), 0) then
+			raise ESocket.Create('Error sending');
+	end;
+
+var
+	LData: String;
+begin
+	if Length(Args.Value) > 0 then
+		LData := String.Join(CSeparator, [Args.Module, Args.Feature, 'w', Args.Value])
+	else
+		LData := String.Join(CSeparator, [Args.Module, Args.Feature, 'r']);
+
+	SocketSend(CHandshake + 'query');
+	SocketSend(LData);
+end;
+
+function CheckResponse(Socket: TSocket): Boolean;
+var
+	LBuffer: Array[0 .. 4000] of Char;
+	LData: String;
+	LRead: Int64;
+begin
+	LRead := fpRecv(Socket, @LBuffer[0], High(LBuffer), 4);
+	result := False;
+	if LRead = -1 then
+		raise ESocket.Create('Error receiving');
+
+	LData := String(PChar(LBuffer));
+	result := Pos(CSuccess, LData) = 1;
+end;
+
+var
+	LSock: TSocket;
+	LRes: Boolean;
+begin
+	LSock := SocketConnect('/var/run/pcrd.sock');
+	LRes := False;
+
+	try try
+		SendQuery(LSock, ReadArguments);
+		LRes := CheckResponse(LSock);
+	except
+		on Ex: Exception do begin
+			Writeln(StdErr, Ex.ToString);
+		end;
+	end;
+	finally
+		fpClose(LSock);
+	end;
+
+	if LRes then
+		Halt(0)
+	else
+		Halt(1);
+end.
+
